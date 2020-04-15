f90e4726c67c5f19bc9e42df4a4644378efb3e57
mill <ph.mill@gmail.com>
2020-04-15 18:30:43 +0200

    Angular-communicating angular 9 and Bootstrap 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+import { ShellComponent } from './home/shell.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      { path: 'welcome', component: WelcomeComponent },
+      {
+        path: 'products',
+        // canActivate: [AuthGuard],
+        // loadChildren: './products/product.module#ProductModule',
+        loadChildren: () =>
+          import('./products/product.module').then((m) => m.ProductModule),
+      },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product List';
+  listFilter: string;
+  showImage: boolean;
+
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  errorMessage: string;
+
+  filteredProducts: IProduct[];
+  products: IProduct[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (products: IProduct[]) => {
+        this.products = products;
+        this.performFilter(this.listFilter);
+      },
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy?: string): void {
+    if (filterBy) {
+      this.filteredProducts = this.products.filter(
+        (product: IProduct) =>
+          product.productName
+            .toLocaleLowerCase()
+            .indexOf(filterBy.toLocaleLowerCase()) !== -1
+      );
+    } else {
+      this.filteredProducts = this.products;
+    }
+  }
+}
